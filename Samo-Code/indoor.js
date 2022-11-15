eb0ba5df1fc9dcdaa1bd9fb562bd1776d900cedf
Gansey14 <hlousek.samuel14@gmail.com>
2022-11-15 12:15:23 +0100

    first pin working

--- Samo-Code/indoor.js
+++ Samo-Code/indoor.js
@@ -1,9 +1,13 @@
-const button1 = document.getElementById("i1");
+const button1 = document.querySelector(".i1");
+const pin1 = document.querySelector(".firstpin");
+
+button1.classList.toggle("iconwrapper");
+button1.classList.toggle("iconwrapper");
+pin1.classList.toggle("pinhidden");
 
 button1.onclick = function () {
+    button1.classList.toggle("iconwrapper");
     button1.classList.toggle("buttonpressed");
+    pin1.classList.toggle("pinhidden");
 }
 
-button1.addEventListener("click", function () {
-    button1.classList.toggle("buttonpressed")
-})
