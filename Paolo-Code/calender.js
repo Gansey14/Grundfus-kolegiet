23b2eea9f723f089b6ce99b915adf531a46cdcda
PapaniaP11 <eaapapap@students.eaaa.dk>
2022-11-16 17:41:15 +0100

    Calender update 10.5

--- Paolo-Code/calender.js
+++ Paolo-Code/calender.js
@@ -23,13 +23,37 @@ const months = [
 ];
 
 const renderCalender = () => {
-  let lastDateofMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // Getting last Date of the month
+  let firstDayofMonth = new Date(currentYear, currentMonth, 1).getDay(), // Getting first day of the month
+    lastDateofMonth = new Date(currentYear, currentMonth + 1, 0).getDate(), // Getting last Date of this month
+    lastDayofMonth = new Date(
+      currentYear,
+      currentMonth,
+      lastDateofMonth
+    ).getDay(), // Getting last day of this month
+    lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate(); // Getting last Date of previous month
   let liTag = "";
 
-  for (let i = 1; i <= lastDateofMonth; i++) {
-    liTag += `<li>${i}</li>`;
+  //creating li of previous month's last days
+  for (let i = firstDayofMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
   }
 
+  //creating li of all days of current month
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    // adding active class to li of the current day - if matches
+    let isToday =
+      i === date.getDate() &&
+      currentMonth === new Date().getMonth() &&
+      currentYear === new Date().getFullYear()
+        ? "active"
+        : "";
+    liTag += `<li class="${isToday}">${i}</li>`;
+  }
+
+  //creating li of first days of next month
+  for (let i = lastDayofMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+  }
   currentDate.innerText = `${months[currentMonth]} ${currentYear}`;
   daysTag.innerHTML = liTag;
 };
@@ -40,6 +64,16 @@ prevNextIcon.forEach((icon) => {
   icon.addEventListener("click", () => {
     // Ternary operator - If clicked icon is prev icon then decrement else increment
     currentMonth = icon.id === "prev" ? currentMonth - 1 : currentMonth + 1;
+
+    // If current month is less than 0 or more than 11
+    if (currentMonth < 0 || currentMonth > 11) {
+      date = new Date(currentYear, currentMonth);
+      currentYear = date.getFullYear();
+      currentMonth = date.getMonth();
+    } else {
+      date = new Date();
+    }
+
     renderCalender();
   });
 });
