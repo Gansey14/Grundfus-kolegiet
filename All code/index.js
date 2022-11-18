efadd0fa5d1f3c5f6ff4d4272cdf07593c6daac2
Gansey14 <hlousek.samuel14@gmail.com>
2022-11-18 01:43:22 +0100

    index linked with other sections in the grid

---
+++ All code/index.js
@@ -0,0 +1,9 @@
+const mapsPart = document.querySelector(".maps-part");
+const dashboard = document.querySelector(".dashboard");
+
+
+
+mapsPart.addEventListener("click", function () {
+    mapsPart.link('https://www.w3schools.com/jsref/event_transitionend.asp');
+});
+
